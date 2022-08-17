de17e15bd1b408acd935840f8663933ceb8b03aa
OHaEuN <ohr0801@naver.com>
2022-08-18 08:40:23 +0900

    Edit Search

--- janbu/src/routes/Community.js
+++ janbu/src/routes/Community.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import axios from "axios";
 
 
 function Community () {
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
+            console.log(res)
+            document.querySelector(".contents").innerHTML=`${res.data[600].Deaths}명 사망`;
+        }
+        fetchEvents();
+    })
+
     return (
-    <>
-        <h1>community</h1>
-    </>
-    );
+        <section>
+            <h2>국내 코로나 현황</h2>
+            <div className="contents">
+            </div>
+        </section>
+    )
   }
 
 export default Community;
