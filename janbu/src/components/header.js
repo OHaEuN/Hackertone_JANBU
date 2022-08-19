514408012ea2cc358edf6813000e6e6c16fc6076
Taesun <liamtsy@icloud.com>
2022-08-19 19:08:07 +0900

    Create Header component

--- janbu/src/components/header.js
+++ janbu/src/components/header.js
@@ -1,30 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-	faUser,
-	faPencil,
-	faMagnifyingGlass,
-	faPenToSquare,
-	faClipboard
-} from '@fortawesome/free-regular-svg-icons';
-
+import { BiSearchAlt } from 'react-icons/bi';
+import { FiUser } from 'react-icons/fi';
+import { BsPencil } from 'react-icons/bs';
+import './Header.css';
 function Header() {
 	return (
-		<div id="headerWrap">
-			<div id="header__top">
-				<h1>잔반을 부탁해</h1>
-				{/* <Search /> */}
-				<div id="header__top__icons">
-					<FontAwesomeIcon icon={faUser} />
-					<FontAwesomeIcon icon={faPenToSquare} />
-					<FontAwesomeIcon icon={faClipboard} />
+		<div id="header">
+			<div id="header__wrap">
+				<div id="header__top">
+					<h1>잔반을 부탁해</h1>
+					<div id="header__top__icons">
+						<BiSearchAlt className="icon" />
+						<BsPencil className="icon" />
+						<FiUser className="icon" />
+					</div>
 				</div>
-			</div>
-			<div id="header__bottom">
-				{/* 레시피 */}
-				{/* 교환&나눔 */}
-				{/* 구매 */}
+				<div id="header__bottom">
+					<div className="menu">레시피</div>
+					<Link className="menu" to="/community">
+						<div>나눔&교환</div>
+					</Link>
+					<Link className="menu" to="/store">
+						<div>구매</div>
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
