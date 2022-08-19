16af69f47ae324bf350433909a9ce50388052d18
OHaEuN <ohr0801@naver.com>
2022-08-19 20:34:30 +0900

    edit searchBar

--- janbu/src/components/SearchBar.js
+++ janbu/src/components/SearchBar.js
@@ -5,14 +5,21 @@ import SearchResult from "./SearchResult";
 function SearchBar(){
   const [keywordArray, addKeywordToArray] = useState([]);
   const [keyword, setKeyword] = useState("");
-  const handleAddClick = () => {
+  const handleAdd = () => {
+    console.log("키워드 배열에 추가함");
     addKeywordToArray([keyword, ...keywordArray]);
   };
   const handleSearchClick = () => {
-    console.log(keywordArray);
 
+    console.log(keywordArray);
   };
   const onChange = (event) => setKeyword(event.target.value);
+  const handleOnKeyPress = (event) => {
+		if (event.key === 'Enter') {
+			handleAdd(); // Enter 입력이 되면 클릭 이벤트 실행
+		}
+	};
+
   
 
   useEffect(() => {
@@ -27,16 +34,16 @@ function SearchBar(){
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="키워드 입력"
-      >
+      <div id = "mainSearchBar">
+        <input
+          value={keyword}
+          onChange={onChange}
+          onKeyPress={handleOnKeyPress}
+          type="text"
+          placeholder="키워드 입력"
+        />
         <button type="submit" onClick={handleSearchClick}>검색</button>
-      </input>
-      <button type="submit" onClick={handleAddClick}>재료 추가</button>
-      
+      </div>      
       <div id="result">
         <SearchResult props={keyword}/>
       </div>
